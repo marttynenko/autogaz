7ab9407afc69086a28225a9aa63d43351b346f1c
emomela <emomela@yandex.ru>
2021-07-03 11:12:53 +0200

    search and popup

---
+++ src/js/script2.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.popup-with-form').magnificPopup( {    
+		closeOnBgClick: true
+  });
+
+
+  $('.header-search').click(function(){
+  $('.header-search-form').addClass('header-search-form-active');   
+});
+
+
+});
+
+
