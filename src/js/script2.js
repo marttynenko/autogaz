c941d199a324c9c46831f42585ff8dcfe98c95ea
marttynenko <marttynenko@gmail.com>
2021-07-03 14:50:25 +0300

    Fix searchdropdown

--- src/js/script2.js
+++ src/js/script2.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-	$('.popup-with-form').magnificPopup( {    
-		closeOnBgClick: true
-  });
+
+    $('.popup-with-form').magnificPopup( {    
+        closeOnBgClick: true
+    });
 
 
-  $('.header-search').click(function(){
-  $('.header-search-form').addClass('header-search-form-active');   
-});
+    $('.header-search').click(function(){
+        $('.header-search-form').addClass('header-search-form-active');   
+    });
+
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $('.header-search'); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            $('.header-search-form').removeClass('header-search-form-active'); // скрываем его
+        }
+    });
 
 
 });
