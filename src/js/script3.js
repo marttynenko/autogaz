df6f8b3c07cb2df2c3d0967177a202e7fd14cd53
emomela <86851827+emomela@users.noreply.github.com>
2021-08-30 22:48:20 +0200

    add: promo page

---
+++ src/js/script3.js
@@ -0,0 +1,32 @@
+$('.promotion-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 970,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+});
